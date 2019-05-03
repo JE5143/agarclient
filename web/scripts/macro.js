5c8fac2a68bd1036a73f4147df0a92fa3dd793bc
JE5143 <48130949+JE5143@users.noreply.github.com>
2019-05-03 21:13:43 +0000

    Update macro.js

--- web/scripts/macro.js
+++ web/scripts/macro.js
@@ -39,8 +39,8 @@ function keyDown(event) {
         for (var a = 0; a < 4; a++) {
             setTimeout(function() {
                 split();
-                $("body").trigger($.Event("keydown", { key: f}));
-                $("body").trigger($.Event("keyup", { key: f}));
+                $("body").trigger($.Event("keydown", { keyCode: 87}));
+                $("body").trigger($.Event("keyup", { keyCode: 87}));
             }, a * 50);
         }
     }
